c1c46f1b86a7a3ca1f64fdf10fd45c7105bbfa07
maryamtahir239 <aqsatahir62@gmail.com>
2024-11-07 12:45:12 +0500

    initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		outDir: "dist",  // Explicitly set the output directory to "dist"
+	},
 	server: {
 		proxy: {
 			"/api": {
